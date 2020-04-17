68d8bfa87174282f8390b90a35d9df658ddcfe21
Oliver Leitner <never@neverslair-blog.net>
2020-04-17 06:20:40 +0200

    make link targets _blank and set generic title

--- src/components/markdowntohtml-component/markdowntohtml-component.tsx
+++ src/components/markdowntohtml-component/markdowntohtml-component.tsx
@@ -17,12 +17,20 @@ export class MarkdowntohtmlComponent {
     );
   }
 
+  // TODO: enable whitelisting, i.e. for the own domain...
+  setLinkStuff() {
+    const html = this.convertToHTML();
+    var urlPattern = /(<a [^>]*(href="([^>^\"]*)")[^>]*>)([^<]+)(<\/a>)/gi;
+    var output = html.replace(urlPattern, '<a $2 title="$4" target="_blank">$4</a>');
+    return output;
+  }
+
   render() {
     marked.default.setOptions({
       highlight: function(code) {
         return hljs.highlightAuto(code).value;
       }
     });
-    return <div innerHTML={this.convertToHTML()}/>;
+    return <div innerHTML={this.setLinkStuff()}/>;
   }
 }
